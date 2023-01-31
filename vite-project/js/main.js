aab17a9b8b2bde2ec0931bc7611f125eac3d3c5a
danielzionlam <111651448+danielzionlam@users.noreply.github.com>
2023-01-31 11:48:05 -0500

    adsf

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,10 +1,22 @@
 import "../styles/style.css";
 let result = document.getElementById("result");
 let searchBtn = document.getElementById("search-btn");
-let url = "https://www.balldontlie.io/api/v1/players";
-let inp = document.getElementById("inp").value;
-fetch(url + "Ike")
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-  });
+let url = "https://www.balldontlie.io/api/v1/teams";
+// fetch(url + "")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+//     const team = data.filter((e) => e.full_name.includes(inp));
+//     console.log(team);
+//   });
+async function search(event) {
+  let inp = document.getElementById("inp").value;
+  console.log(inp);
+  event.preventDefault();
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  const team = data.data.filter((e) => e.full_name.includes(inp));
+  console.log(team);
+}
+searchBtn.addEventListener("click", search);
