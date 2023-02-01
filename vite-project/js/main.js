da4102bbaeffd5e4c98cb5e612901bc4b7978bf5
danielzionlam <111651448+danielzionlam@users.noreply.github.com>
2023-02-01 11:30:32 -0500

    rsg

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -36,27 +36,37 @@ async function search(teams) {
     UTA: "https://assets.stickpng.com/images/58419bb6a6515b1e0ad75a55.png",
     WAS: "https://assets.stickpng.com/images/58419c12a6515b1e0ad75a5b.png",
   };
-  let inp = document.getElementById("inp").value;
-  console.log(inp);
-  teams.preventDefault();
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log(data);
-  const team = data.data.filter((e) => e.full_name.includes(inp));
-  console.log("team");
-  console.log(team);
-  result.innerText = "";
-  result.insertAdjacentHTML(
-    "afterbegin",
-    `<div class="card">
-    <img class="card-pic" src="${dict[team[0].abbreviation]}">
-    <div class="card-text">
-      <h4 class="card-title">${team[0].full_name}</h4>
-      <h4 class="card-title">City: ${team[0].city}</h4>
-      <h4 class="card-title">Division: ${team[0].division}</h4>
-      <h4 class="card-title">Conference: ${team[0].conference}</h4>
-    </div>
-  </div>`
-  );
+  try {
+    let inp = document.getElementById("inp").value;
+    console.log(inp);
+    teams.preventDefault();
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    const team = data.data.filter((e) => e.full_name.includes(inp));
+    if (inp.length === 0 || team.length === 0) {
+      throw new Error(
+        "Invalid Input. Try capitalizing the first letter. ex:Celtics"
+      );
+      return;
+    }
+    console.log("team");
+    console.log(team);
+    result.innerText = "";
+    result.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="card">
+      <img class="card-pic" src="${dict[team[0].abbreviation]}">
+      <div class="card-text">
+        <h4 class="card-title">${team[0].full_name}</h4>
+        <h4 class="card-title">City: ${team[0].city}</h4>
+        <h4 class="card-title">Division: ${team[0].division}</h4>
+        <h4 class="card-title">Conference: ${team[0].conference}</h4>
+      </div>
+    </div>`
+    );
+  } catch (error) {
+    alert(error);
+  }
 }
 searchBtn.addEventListener("click", search);
